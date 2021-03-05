52a2e7c501be221d6ff91de62a5bf2bf4e22b2c7
Jaro Regec <jregec@Jaros-MacBook-Pro.local>
2021-03-05 09:10:49 +0100

    added non streaming methods to codec

--- core/src/main/scala/zio/schema/codec/Codec.scala
+++ core/src/main/scala/zio/schema/codec/Codec.scala
@@ -2,8 +2,12 @@ package zio.schema.codec
 
 import zio.schema._
 import zio.stream.ZTransducer
+import zio.Chunk
 
 trait Codec {
   def encoder[A](schema: Schema[A]): ZTransducer[Any, Nothing, A, Byte]
   def decoder[A](schema: Schema[A]): ZTransducer[Any, String, Byte, A]
+
+  def encode[A](schema: Schema[A]): A => Chunk[Byte]
+  def decode[A](schema: Schema[A]): Chunk[Byte] => Either[String, A]
 }
