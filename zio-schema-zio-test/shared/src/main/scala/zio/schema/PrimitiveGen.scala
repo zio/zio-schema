b9b7fe0bd58041a314c89ed563f2e67b9584e9c9
Ivan Grebenik <vanya.grebenik@gmail.com>
2021-05-27 00:11:08 +0300

    update java time generators

--- zio-schema-zio-test/shared/src/main/scala/zio/schema/PrimitiveGen.scala
+++ zio-schema-zio-test/shared/src/main/scala/zio/schema/PrimitiveGen.scala
@@ -1,12 +1,14 @@
 package zio.schema
 
+import zio.random.Random
+import zio.test._
+
 import java.math.{ BigDecimal, BigInteger }
 import java.time._
 
-import scala.jdk.CollectionConverters._
+import scala.collection.JavaConverters._
 
-import zio.random.Random
-import zio.test._
+import com.github.ghik.silencer.silent
 
 object PrimitiveGen {
 
@@ -33,96 +35,41 @@ object PrimitiveGen {
       case _: StandardType.ZoneId.type         => anyZoneId
       case _: StandardType.ZoneOffset.type     => anyZoneOffset
       case _: StandardType.Duration            => anyDuration
-      case _: StandardType.Instant             => anyInstant
+      case _: StandardType.Instant             => Gen.anyInstant
       case _: StandardType.LocalDate           => anyLocalDate
       case _: StandardType.LocalTime           => anyLocalTime
-      case _: StandardType.LocalDateTime       => anyLocalDateTime
+      case _: StandardType.LocalDateTime       => Gen.anyLocalDateTime
       case _: StandardType.OffsetTime          => anyOffsetTime
-      case _: StandardType.OffsetDateTime      => anyOffsetDateTime
+      case _: StandardType.OffsetDateTime      => Gen.anyOffsetDateTime
       case _: StandardType.ZonedDateTime       => anyZonedDateTime
     }
 
-  val anyDayOfWeek: Gen[Random, DayOfWeek] =
-    Gen.oneOf(
-      Gen.const(DayOfWeek.MONDAY),
-      Gen.const(DayOfWeek.TUESDAY),
-      Gen.const(DayOfWeek.WEDNESDAY),
-      Gen.const(DayOfWeek.THURSDAY),
-      Gen.const(DayOfWeek.FRIDAY),
-      Gen.const(DayOfWeek.SATURDAY),
-      Gen.const(DayOfWeek.SUNDAY)
+  def anyDayOfWeek: Gen[Random, DayOfWeek] =
+    Gen.elements(
+      DayOfWeek.MONDAY,
+      DayOfWeek.TUESDAY,
+      DayOfWeek.WEDNESDAY,
+      DayOfWeek.THURSDAY,
+      DayOfWeek.FRIDAY,
+      DayOfWeek.SATURDAY,
+      DayOfWeek.SUNDAY
     )
 
-  val anyMonth: Gen[Random, Month] =
-    Gen.oneOf(
-      Gen.const(Month.JANUARY),
-      Gen.const(Month.FEBRUARY),
-      Gen.const(Month.MARCH),
-      Gen.const(Month.APRIL),
-      Gen.const(Month.MAY),
-      Gen.const(Month.JUNE),
-      Gen.const(Month.JULY),
-      Gen.const(Month.AUGUST),
-      Gen.const(Month.SEPTEMBER),
-      Gen.const(Month.OCTOBER),
-      Gen.const(Month.NOVEMBER),
-      Gen.const(Month.DECEMBER)
-    )
-
-  val anyMonthDay: Gen[Random, MonthDay] =
-    for {
-      month <- anyMonth
-      days  <- Gen.int(1, month.maxLength())
-    } yield MonthDay.of(month, days)
-
-  val anyPeriod: Gen[Random, Period] =
-    for {
-      years  <- Gen.int(0, Int.MaxValue)
-      months <- Gen.int(0, Int.MaxValue)
-      days   <- Gen.int(0, Int.MaxValue)
-    } yield Period.of(years, months, days)
-
-  val anyYear: Gen[Random, Year] =
-    Gen.int(Year.MIN_VALUE, Year.MAX_VALUE).map(Year.of)
-
-  val anyYearMonth: Gen[Random, YearMonth] =
-    for {
-      year  <- anyYear
-      month <- anyMonth
-    } yield YearMonth.of(year.getValue(), month.getValue())
-
-  val anyZoneId: Gen[Random, ZoneId] = {
-    val zoneIds = ZoneId.SHORT_IDS.keySet().asScala
-    val idMap   = Set.range(0, zoneIds.size).zip(zoneIds).toMap
-
-    Gen.int(0, zoneIds.size).map(idx => ZoneId.of(idMap(idx)))
-  }
-
-  val anyZoneOffset: Gen[Random, ZoneOffset] =
-    anyZoneId.map { zoneId =>
-      val instant = Instant.now()
-      zoneId.getRules().getOffset(instant)
-    }
-
-  val anyDuration: Gen[Random, Duration] =
+  def anyDuration: Gen[Random, Duration] =
     for {
       seconds <- Gen.anyLong
       nanos   <- Gen.long(0, 999999999L)
     } yield Duration.ofSeconds(seconds, nanos)
 
-  val anyInstant: Gen[Random, Instant] =
+  def anyLocalDate: Gen[Random, LocalDate] =
     for {
-      seconds <- Gen.long(Instant.MIN.getEpochSecond(), Instant.MAX.getEpochSecond())
-      nanos   <- Gen.int(Instant.MIN.getNano(), Instant.MAX.getNano())
-    } yield Instant.ofEpochSecond(seconds, nanos.toLong)
+      year   <- anyYear
+      month  <- Gen.int(1, 12)
+      maxLen = if (!year.isLeap && month == 2) 28 else Month.of(month).maxLength
+      day    <- Gen.int(1, maxLen)
+    } yield LocalDate.of(year.getValue, month, day)
 
-  val anyLocalDate: Gen[Random, LocalDate] =
-    for {
-      yaer     <- anyYear
-      monthDay <- anyMonthDay
-    } yield LocalDate.of(yaer.getValue(), monthDay.getMonth(), monthDay.getDayOfMonth())
-
-  val anyLocalTime: Gen[Random, LocalTime] =
+  def anyLocalTime: Gen[Random, LocalTime] =
     for {
       hour   <- Gen.int(0, 23)
       minute <- Gen.int(0, 59)
@@ -130,36 +77,63 @@ object PrimitiveGen {
       nanos  <- Gen.int(0, 999999999)
     } yield LocalTime.of(hour, minute, second, nanos)
 
-  val anyLocalDateTime: Gen[Random, LocalDateTime] =
-    for {
-      date <- anyLocalDate
-      time <- anyLocalTime
-    } yield LocalDateTime.of(
-      date.getYear(),
-      date.getMonth(),
-      date.getDayOfMonth(),
-      time.getHour(),
-      time.getMinute(),
-      time.getSecond(),
-      time.getNano()
+  def anyMonth: Gen[Random, Month] =
+    Gen.elements(
+      Month.JANUARY,
+      Month.FEBRUARY,
+      Month.MARCH,
+      Month.APRIL,
+      Month.MAY,
+      Month.JUNE,
+      Month.JULY,
+      Month.AUGUST,
+      Month.SEPTEMBER,
+      Month.OCTOBER,
+      Month.NOVEMBER,
+      Month.DECEMBER
     )
 
-  val anyOffsetTime: Gen[Random, OffsetTime] =
+  def anyMonthDay: Gen[Random, MonthDay] =
+    for {
+      month <- Gen.int(1, 12).map(Month.of)
+      days  <- Gen.int(1, month.maxLength())
+    } yield MonthDay.of(month, days)
+
+  def anyOffsetTime: Gen[Random, OffsetTime] =
     for {
       time   <- anyLocalTime
       offset <- anyZoneOffset
-    } yield OffsetTime.of(time, offset)
+    } yield OffsetTime.of(time, ZoneOffset.ofTotalSeconds(-offset.getTotalSeconds))
 
-  val anyOffsetDateTime: Gen[Random, OffsetDateTime] =
+  def anyPeriod: Gen[Random, Period] =
     for {
-      dateTime <- anyLocalDateTime
-      offset   <- anyZoneOffset
-    } yield OffsetDateTime.of(dateTime, offset)
+      years  <- Gen.int(0, Int.MaxValue)
+      months <- Gen.int(0, Int.MaxValue)
+      days   <- Gen.int(0, Int.MaxValue)
+    } yield Period.of(years, months, days)
 
-  val anyZonedDateTime: Gen[Random, ZonedDateTime] =
+  def anyYear: Gen[Random, Year] =
+    Gen.int(Year.MIN_VALUE, Year.MAX_VALUE).map(Year.of)
+
+  def anyYearMonth: Gen[Random, YearMonth] =
     for {
-      dateTime <- anyLocalDateTime
+      year  <- anyYear
+      month <- Gen.int(1, 12)
+    } yield YearMonth.of(year.getValue(), month)
+
+  def anyZonedDateTime: Gen[Random, ZonedDateTime] =
+    for {
+      dateTime <- Gen.anyLocalDateTime
       zoneId   <- anyZoneId
     } yield ZonedDateTime.of(dateTime, zoneId)
+
+  @silent("JavaConverters")
+  final def anyZoneId: Gen[Random, ZoneId] =
+    Gen.elements(ZoneId.getAvailableZoneIds.asScala.map(ZoneId.of).toList: _*).noShrink
+
+  def anyZoneOffset: Gen[Random, ZoneOffset] =
+    Gen
+      .int(ZoneOffset.MIN.getTotalSeconds, ZoneOffset.MAX.getTotalSeconds)
+      .map(ZoneOffset.ofTotalSeconds)
 
 }
