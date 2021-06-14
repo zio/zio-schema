200fe6ba4835f678fb6283b9171f74c4453457ea
thinkharder <thinkharderdev@users.noreply.github.com>
2021-06-14 19:14:20 -0400

    Fixes #78: Implement diffing between instances described by a schema

--- zio-schema/shared/src/main/scala/zio/schema/syntax.scala
+++ zio-schema/shared/src/main/scala/zio/schema/syntax.scala
@@ -5,14 +5,5 @@ object syntax extends SchemaSyntax
 trait SchemaSyntax {
   implicit class DiffOps[A: Schema](a: A) {
     def diff(that: A): Diff = Schema[A].diff(a, that)
-
-    /**
-     * alias for diff that does not conflict with scala stdlib
-     */
-    def diffEach(that: A): Diff = Schema[A].diff(a, that)
-  }
-
-  implicit class DynamicValueOps[A: Schema](a: A) {
-    def dynamic: DynamicValue = Schema[A].toDynamic(a)
   }
 }
