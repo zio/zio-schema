507d9000c03a87e7fef1765e990ef3d87b624ca2
thinkharder <thinkharderdev@users.noreply.github.com>
2021-10-16 10:28:42 -0400

    Progress on recursive derivation. The only thing not working yet (for case classes) is mutual recursion"

--- zio-schema-macros/shared/src/main/scala/zio/schema/SchemaDerivation.scala
+++ zio-schema-macros/shared/src/main/scala/zio/schema/SchemaDerivation.scala
@@ -1,93 +1,175 @@
 package zio.schema
 
+import zio.Chunk
+
+import scala.reflect.macros.whitebox
+
 object SchemaDerivation {
   import scala.language.experimental.macros
-  import scala.reflect.macros.whitebox.Context
 
   implicit def gen[T]: Schema[T] = macro genImpl[T]
 
-  def genImpl[T: c.WeakTypeTag](c: Context): c.Tree = {
+  def genImpl[T: c.WeakTypeTag](c: whitebox.Context): c.Tree = {
     import c.universe._
 
-    val _tag         = c.weakTypeTag[T]
-    val tpe          = weakTypeOf[T]
-    val tpeCompanion = tpe.typeSymbol.companion
+    val tpe = weakTypeOf[T]
 
-    val fieldTypes: Iterable[TermSymbol] = tpe.decls.collect {
-      case p: TermSymbol if p.isCaseAccessor && !p.isMethod => p
-    }
-
-    val schemaType = {
-      // val arity = fieldTypes.size
-      // if (arity <= 22) {
-      //   val typeArgs = fieldTypes.map(ft => q"${tpe.decl(ft.name).typeSignature}") ++ Iterable(q"$tpe")
-      //   val className = q"zio.schema.Schema.CaseClass$arity"
-      //   val clazz = tq"$className"
-      //   q"$clazz[..$typeArgs]"
-      // } else {
-      //   c.abort(tpe.termSymbol.pos, "GenericRecord not supported yet")
-      // }
-      fieldTypes.size match {
-        case 1 =>
-          val typeArgs = fieldTypes.map(ft => q"${tpe.decl(ft.name).typeSignature}") ++ Iterable(q"$tpe")
-          q"zio.schema.Schema.CaseClass1[..$typeArgs]"
-        case 2 =>
-          val typeArgs = fieldTypes.map(ft => q"${tpe.decl(ft.name).typeSignature}") ++ Iterable(q"$tpe")
-          q"zio.schema.Schema.CaseClass2[..$typeArgs]"
-        case 3 =>
-          val typeArgs = fieldTypes.map(ft => q"${tpe.decl(ft.name).typeSignature}") ++ Iterable(q"$tpe")
-          q"zio.schema.Schema.CaseClass3[..$typeArgs]"
-        case _ => c.abort(tpe.termSymbol.pos, "Only handling case class 3 right now")
+    def resolveTermSchema(term: TermSymbol): Tree =
+      if (term.typeSignature =:= tpe) q"zio.schema.Schema.defer(v)"
+      else if (term.typeSignature.typeArgs.exists(_ =:= tpe)) {
+        if (term.typeSignature <:< typeOf[Option[_]])
+          q"""
+             zio.schema.Schema.defer(Schema.option(v))
+           """
+        else if (term.typeSignature <:< typeOf[List[_]])
+          q"""
+             zio.schema.Schema.defer(Schema.list(v))
+           """
+        else if (term.typeSignature <:< typeOf[Set[_]])
+          q"""
+             zio.schema.Schema.defer(Schema.set(v))
+           """
+        else if (term.typeSignature <:< typeOf[Vector[_]])
+          q"""
+             zio.schema.Schema.defer(Schema.vector(v))
+           """
+        else if (term.typeSignature <:< typeOf[Chunk[_]])
+          q"""
+             zio.schema.Schema.defer(Schema.chunk(v))
+           """
+        else
+          c.abort(c.enclosingPosition, s"Cannot resolve schema for type ${show(term.typeSignature)}")
+      } else {
+        c.inferImplicitValue(c.typecheck(tq"zio.schema.Schema[$term]", c.TYPEmode).tpe, withMacrosDisabled = false)
       }
+    //      else q"zio.schema.Schema.defer(${c.inferImplicitValue(c.typecheck(fieldSchemaType, c.TYPEmode).tpe)})"
+
+    def deriveRecord(tpe: Type): c.Tree = {
+      val tpeCompanion = tpe.typeSymbol.companion
+
+      val fieldTypes: Iterable[TermSymbol] = tpe.decls.collect {
+        case p: TermSymbol if p.isCaseAccessor && !p.isMethod => p
+      }
+      val typeArgs = fieldTypes.map(ft => q"${tpe.decl(ft.name).typeSignature}") ++ Iterable(q"$tpe")
+
+      val schemaType = {
+        fieldTypes.size match {
+          case 1 => q"zio.schema.Schema.CaseClass1[..$typeArgs]"
+          case 2 => q"zio.schema.Schema.CaseClass2[..$typeArgs]"
+          case 3 => q"zio.schema.Schema.CaseClass3[..$typeArgs]"
+          case 4 => q"zio.schema.Schema.CaseClass4[..$typeArgs]"
+          case 5 => q"zio.schema.Schema.CaseClass5[..$typeArgs]"
+          case 6 => q"zio.schema.Schema.CaseClass6[..$typeArgs]"
+          case 7 => q"zio.schema.Schema.CaseClass7[..$typeArgs]"
+          case 8 => q"zio.schema.Schema.CaseClass8[..$typeArgs]"
+          case 9 => q"zio.schema.Schema.CaseClass9[..$typeArgs]"
+          case _ =>
+            c.abort(
+              tpe.termSymbol.pos,
+              s"Only handling case class 3 right now but got ${show(tpe)}, ${show(tpeCompanion)}"
+            )
+        }
+      }
+
+      val fieldLabels = tpe.decls.collect {
+        case p: TermSymbol if p.isCaseAccessor && !p.isMethod => p.name.toString.trim()
+      }
+
+      val fieldDefs = fieldLabels.zip(fieldTypes).zipWithIndex.map {
+        case ((label, termSymbol), idx) =>
+          val fieldSchema = resolveTermSchema(termSymbol)
+          val fieldArg    = if (fieldTypes.size > 1) TermName(s"field${idx + 1}") else TermName("field")
+          val fieldLabel  = label
+
+          q"""$fieldArg = zio.schema.Schema.Field[${termSymbol.typeSignature}]($fieldLabel,$fieldSchema)"""
+      }
+
+      val constructArgs = fieldTypes.zipWithIndex.map {
+        case (term, idx) =>
+          val arg = TermName(s"_$idx")
+          q"$arg: ${term.typeSignature}"
+      }
+      val constructApplyArgs = fieldTypes.zipWithIndex.map {
+        case (_, idx) =>
+          val arg = TermName(s"_$idx")
+          q"$arg"
+      }
+
+      val constructExpr = q"construct = (..$constructArgs) => $tpeCompanion(..$constructApplyArgs)"
+
+      val extractors = fieldLabels.zipWithIndex.foldLeft(Seq.empty[c.universe.Tree]) {
+        case (acc, (fieldLabel, idx)) =>
+          val argName = if (fieldTypes.size > 1) TermName(s"extractField${idx + 1}") else TermName("extractField")
+          acc :+ q"$argName = (t: $tpe) => t.${TermName(fieldLabel)}"
+      }
+
+      val applyArgs = Iterable(q"annotations = zio.Chunk.empty") ++ fieldDefs ++ Iterable(constructExpr) ++ extractors
+
+      val instance = fieldTypes.size match {
+        case 1 =>
+          q"""{
+            lazy val v: zio.schema.Schema.CaseClass1[..$typeArgs] = $schemaType(..$applyArgs)
+            v
+          }"""
+        case 2 =>
+          q"""{
+            lazy val v: zio.schema.Schema.CaseClass2[..$typeArgs] = $schemaType(..$applyArgs)
+            v
+          }"""
+        case 3 =>
+          q"""{
+            lazy val v: zio.schema.Schema.CaseClass3[..$typeArgs] = $schemaType(..$applyArgs)
+            v
+          }"""
+        case 4 =>
+          q"""{
+            lazy val v: zio.schema.Schema.CaseClass4[..$typeArgs] = $schemaType(..$applyArgs)
+            v
+          }"""
+        case 5 =>
+          q"""{
+            lazy val v: zio.schema.Schema.CaseClass5[..$typeArgs] = $schemaType(..$applyArgs)
+            v
+          }"""
+        case 6 =>
+          q"""{
+            lazy val v: zio.schema.Schema.CaseClass6[..$typeArgs] = $schemaType(..$applyArgs)
+            v
+          }"""
+        case 7 =>
+          q"""{
+            lazy val v: zio.schema.Schema.CaseClass7[..$typeArgs] = $schemaType(..$applyArgs)
+            v
+          }"""
+        case 8 =>
+          q"""{
+            lazy val v: zio.schema.Schema.CaseClass8[..$typeArgs] = $schemaType(..$applyArgs)
+            v
+          }"""
+        case 9 =>
+          q"""{
+            lazy val v: zio.schema.Schema.CaseClass9[..$typeArgs] = $schemaType(..$applyArgs)
+            v
+          }"""
+        case _ =>
+          c.abort(
+            tpe.termSymbol.pos,
+            s"Only handling case class 3 right now but got ${show(tpe)}, ${show(tpeCompanion)}"
+          )
+      }
+
+//      println(s"Instance:\n${show(instance)}")
+
+      instance
+
     }
 
-    val fieldLabels = _tag.tpe.decls.collect {
-      case p: TermSymbol if p.isCaseAccessor && !p.isMethod => p.name.toString().trim()
-    }
+    def deriveEnum(tpe: Type): c.Tree = ???
 
-    val fieldDefs = fieldLabels.zip(fieldTypes).zipWithIndex.map {
-      case ((label, termSymbol), idx) =>
-        val fieldSchemaType = tq"zio.schema.Schema[$termSymbol]"
-        val fieldSchema     = c.inferImplicitValue(c.typecheck(fieldSchemaType, c.TYPEmode).tpe)
-        val fieldArg        = if (fieldTypes.size > 1) TermName(s"field${idx + 1}") else TermName("field")
-        val fieldLael       = label
-
-        q"""$fieldArg = zio.schema.Schema.Field[${termSymbol.typeSignature}]($fieldLael,$fieldSchema)"""
-    }
-
-    println(s"Field Defs:\n${fieldDefs.mkString("\n")}")
-
-    val constructArgs = fieldTypes.zipWithIndex.map {
-      case (term, idx) =>
-        val arg = TermName(s"_$idx")
-        q"$arg: ${term.typeSignature}"
-    }
-    val constructApplyArgs = fieldTypes.zipWithIndex.map {
-      case (_, idx) =>
-        val arg = TermName(s"_$idx")
-        q"$arg"
-    }
-
-    val constructExpr = q"construct = (..$constructArgs) => $tpeCompanion(..$constructApplyArgs)"
-
-    println(s"Constructor: $constructExpr")
-
-    val extractors = fieldLabels.zipWithIndex.foldLeft(Seq.empty[c.universe.Tree]) {
-      case (acc, (fieldLabel, idx)) =>
-        val argName = if (fieldTypes.size > 1) TermName(s"extractField${idx + 1}") else TermName("extractField")
-        acc :+ q"$argName = (t: $tpe) => t.${TermName(fieldLabel)}"
-    }
-
-    println(s"Extractors:\n${extractors.mkString("\n")}")
-
-    val applyArgs = Iterable(q"annotations = zio.Chunk.empty") ++ fieldDefs ++ Iterable(constructExpr) ++ extractors
-
-    val instance = q"$schemaType(..$applyArgs)"
-
-    println(s"Instance:\n$instance")
-
-    // c.Expr(instance)
-    instance
+    if (tpe.typeSymbol.asClass.isModuleClass) q"zio.schema.Schema.singleton(${tpe.typeSymbol.asClass.module})"
+    else if (tpe.typeSymbol.asClass.isCaseClass) deriveRecord(tpe)
+    else if (tpe.typeSymbol.asClass.isTrait && tpe.typeSymbol.asClass.isSealed) deriveEnum(tpe)
+    else c.abort(c.enclosingPosition, "Can only derive Schema for case class or sealed trait")
   }
 
 }
--- zio-schema-macros/shared/src/test/scala/zio/schema/SchemaDerivationSpec.scala
+++ zio-schema-macros/shared/src/test/scala/zio/schema/SchemaDerivationSpec.scala
@@ -1,43 +1,85 @@
 package zio.schema
 
+import zio.Chunk
+
 import scala.annotation.Annotation
 
-import zio.Chunk
+//import zio.Chunk
 // import zio.schema.DeriveSchema._
-import zio.schema.SchemaAssertions.hasSameSchema
+//import zio.schema.SchemaAssertions.hasSameSchema
 // import zio.test.Assertion._
 import zio.test._
 
 object SchemaDerivationSpec extends DefaultRunnableSpec {
-  import SchemaDerivation._
+  import Assertion._
+//  import SchemaDerivation._
 
   final case class annotation1(value: String) extends Annotation
   final case class annotation2(value: String) extends Annotation
 
-  sealed case class UserId(id: String) 
+  sealed case class UserId(id: String)
+
   object UserId {
-    implicit val schema: Schema[UserId] = gen[UserId]
+    implicit lazy val schema: Schema[UserId] = SchemaDerivation.gen[UserId]
+  }
+
+  final case class ContainerFields(field1: Option[String], field2: List[String])
+
+  object ContainerFields {
+    implicit lazy val schema: Schema[ContainerFields] = SchemaDerivation.gen[ContainerFields]
   }
 
   sealed case class User(name: String, @annotation1("foo") @annotation2("bar") id: UserId)
 
-  sealed trait Status
-  case class Ok(response: List[String]) extends Status
-  case class Failed(code: Int, reason: String, additionalExplanation: Option[String], remark: String = "oops")
-      extends Status
-  case object Pending extends Status
+  object User {
+    implicit lazy val schema = SchemaDerivation.gen[User]
+  }
 
-  sealed trait OneOf
-  case class StringValue(value: String)   extends OneOf
-  case class IntValue(value: Int)         extends OneOf
-  case class BooleanValue(value: Boolean) extends OneOf
+//  sealed trait Status
+//  case class Ok(response: List[String]) extends Status
+//  case class Failed(code: Int, reason: String, additionalExplanation: Option[String], remark: String = "oops")
+//      extends Status
+//  case object Pending extends Status
+//
+//  sealed trait OneOf
+//  case class StringValue(value: String)   extends OneOf
+//  case class IntValue(value: Int)         extends OneOf
+//  case class BooleanValue(value: Boolean) extends OneOf
+//
+//  case object Singleton
+//  implicit val singletonSchema = gen[Singleton.type]
+//
+  case class Recursive(
+    field1: Int,
+    field2: Option[Recursive] = None,
+    field3: List[Recursive] = Nil,
+    field4: Set[Recursive] = Set.empty,
+    field5: Vector[Recursive] = Vector.empty,
+    field6: Chunk[Recursive] = Chunk.empty
+  )
 
-  case object Singleton
+  object Recursive {
+    implicit lazy val schema: Schema[Recursive] = SchemaDerivation.gen[Recursive]
+  }
+//
+  case class Cyclic(field1: Long, child: CyclicChild1)
+  case class CyclicChild1(field1: Int, child: CyclicChild2)
+  case class CyclicChild2(field1: String, recursive: Option[Cyclic])
 
-  case class Recursive(field: Option[Recursive])
-
-  case class DependsOnA(a: DependsOnB)
-  case class DependsOnB(b: DependsOnA)
+  object Cyclic {
+    implicit lazy val schema = SchemaDerivation.gen[Cyclic]
+  }
+//
+//  case class DependsOnA(a: DependsOnB)
+//
+//  object DependsOnA {
+//    implicit lazy val schema: Schema[DependsOnA] = gen[DependsOnA]
+//  }
+//  case class DependsOnB(b: DependsOnA)
+//
+//  object DependsOnB {
+//    implicit lazy val schema: Schema[DependsOnB] = gen[DependsOnB]
+//  }
 
   sealed trait Tree[+A]
   case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
@@ -54,46 +96,55 @@ object SchemaDerivationSpec extends DefaultRunnableSpec {
 
   override def spec: ZSpec[Environment, Failure] = suite("DeriveSchemaSpec")(
     suite("Derivation")(
-      test("correctly derives schema for UserId case class") {
-        val derived: Schema[UserId] = Schema[UserId]
-        val expected: Schema[UserId] =
-          Schema.CaseClass1(
-            annotations = Chunk.empty,
-            field = Schema.Field("id", Schema.Primitive(StandardType.StringType)),
-            UserId.apply,
-            (uid: UserId) => uid.id
-          )
-
-        assert(derived)(hasSameSchema(expected))
+      test("case class") {
+        assert(Schema[User].toString)(not(containsString("null")))
       },
+      test("recursive data structure") {
+        assert(Schema[Recursive].toString)(not(containsString("null")))
+      },
+      test("cyclic recursion") {
+        assert(Schema[Cyclic].toString)(not(containsString("null")))
+      }
+      // test("correctly derives schema for UserId case class") {
+      //   val derived: Schema[UserId] = Schema[UserId]
+      //   val expected: Schema[UserId] =
+      //     Schema.CaseClass1(
+      //       annotations = Chunk.empty,
+      //       field = Schema.Field("id", Schema.Primitive(StandardType.StringType)),
+      //       UserId.apply,
+      //       (uid: UserId) => uid.id
+      //     )
+
+      //   assert(derived)(hasSameSchema(expected))
+      // },
       // test("correctly derives schema for Singleton case object") {
       //   val derived: Schema[Singleton.type]  = Schema[Singleton.type]
       //   val expected: Schema[Singleton.type] = Schema[Unit].transform(_ => Singleton, _ => ())
 
       //   assert(derived)(hasSameSchema(expected))
       // } @@ TestAspect.ignore,
-      test("correctly derives schema for case class with nested case classes") {
-        val derived: Schema[User] = Schema[User]
-        val expected: Schema[User] =
-          Schema.CaseClass2(
-            annotations = Chunk.empty,
-            field1 = Schema.Field("name", Schema.Primitive(StandardType.StringType)),
-            field2 = Schema.Field(
-              "id",
-              Schema.CaseClass1(
-                annotations = Chunk.empty,
-                field = Schema.Field("id", Schema.Primitive(StandardType.StringType)),
-                UserId.apply,
-                (uid: UserId) => uid.id
-              ),
-              Chunk(annotation1("foo"), annotation2("bar"))
-            ),
-            User.apply,
-            (u: User) => u.name,
-            (u: User) => u.id
-          )
-        assert(derived)(hasSameSchema(expected))
-      } @@ TestAspect.ignore,
+      // test("correctly derives schema for case class with nested case classes") {
+      //   val derived: Schema[User] = Schema[User]
+      //   val expected: Schema[User] =
+      //     Schema.CaseClass2(
+      //       annotations = Chunk.empty,
+      //       field1 = Schema.Field("name", Schema.Primitive(StandardType.StringType)),
+      //       field2 = Schema.Field(
+      //         "id",
+      //         Schema.CaseClass1(
+      //           annotations = Chunk.empty,
+      //           field = Schema.Field("id", Schema.Primitive(StandardType.StringType)),
+      //           UserId.apply,
+      //           (uid: UserId) => uid.id
+      //         ),
+      //         Chunk(annotation1("foo"), annotation2("bar"))
+      //       ),
+      //       User.apply,
+      //       (u: User) => u.name,
+      //       (u: User) => u.id
+      //     )
+      //   assert(derived)(hasSameSchema(expected))
+      // } @@ TestAspect.ignore,
       // test("correctly derives schema for complex ADT") {
       //   val derived: Schema[Status] = Schema[Status]
       //   val expected: Schema[Status] =
