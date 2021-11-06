cd24ba3f11677b612837f604036ae1c692d816b6
Dominik Dorn <dominik@dominikdorn.com>
2021-11-06 23:45:33 +0100

    [zio-schema-examples] Example5 diffing

--- zio-schema-examples/README.md
+++ zio-schema-examples/README.md
@@ -19,7 +19,10 @@ transform the JSON of a PersonDTO (e.g. from a REST API) into a Person (e.g. for
 To do this, we'll transform the Schema of the PersonDTO into a Schema of the Person.
 
 
-## Example 4
+## Example 4 (not working at the moment)
 In this example we'll take a look on how to use ZIO-Schema to migrate between different objects, 
 e.g. a `WebPerson` to a `DomainPerson`
 
+## Example 5
+In this example we'll see how we can use ZIO-Schema to create Diffs between two objects. This is helpful 
+e.g. to only persist changes to objects, e.g. when using EventSourcing.
---
+++ zio-schema-examples/src/main/scala/dev/zio/schema/example/example5/Example5_Diffing.scala
@@ -0,0 +1,52 @@
+package dev.zio.schema.example.example5
+
+import zio.schema.Schema
+import zio.schema.ast.SchemaAst
+import zio.{ExitCode, URIO, ZIO}
+
+/**
+ * Example 5: In this example, we use ZIO-Schema to detect changes in our objects.
+ *
+ */
+private[example5] object Domain {
+  final case class Person(name: String, age: Int)
+  object Person {
+    val name = Schema.Field[String]("name", Schema.primitive[String])
+    val age  = Schema.Field[Int]("age", Schema.primitive[Int])
+    val schema: Schema[Person] = Schema.CaseClass2[String, Int, Person](
+      field1 = name,
+      field2 = age,
+      construct = (name, age) => Person(name, age),
+      extractField1 = p => p.name,
+      extractField2 = p => p.age
+    )
+  }
+
+  final case class PersonDTO(firstname: String, lastname: String, years: Int)
+  object PersonDTO {
+    val firstname = Schema.Field("firstname", Schema.primitive[String])
+    val lastname  = Schema.Field("lastname", Schema.primitive[String])
+    val years     = Schema.Field("years", Schema.primitive[Int])
+    val schema: Schema[PersonDTO] = Schema.CaseClass3[String, String, Int, PersonDTO](
+      field1 = firstname,
+      field2 = lastname,
+      field3 = years,
+      construct = (fn, ln, y) => PersonDTO(fn, ln, y),
+      extractField1 = _.firstname,
+      extractField2 = _.lastname,
+      extractField3 = _.years
+    )
+  }
+
+}
+
+object Example5_Diffing extends zio.App {
+
+  import Domain._
+
+  val personDTO = PersonDTO("Mike", "Moe", 32)
+
+  val diff = PersonDTO.schema.diff(personDTO, personDTO.copy(lastname = "Max"))
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = ZIO.debug(diff).exitCode
+}
