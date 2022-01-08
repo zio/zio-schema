cc6184b0359c3ae1a161b456806d568cd01cd0c1
Javier Moreno <kvltd00d@gmail.com>
2022-01-08 11:54:23 -0500

    WIP - adding anyOf and allOf operations

--- zio-schema-validation/shared/src/main/scala/zio/schema/validation/ZioSchemaValidation.scala
+++ zio-schema-validation/shared/src/main/scala/zio/schema/validation/ZioSchemaValidation.scala
@@ -28,47 +28,99 @@ object ZioSchemaValidation {
 
   sealed trait Regex
 
-  // String => Boolean
-  sealed trait StringP[A]
-  object StringP {
-    final case class MinLength(n: Int) extends StringP[String]
-    final case class MaxLength(n: Int) extends StringP[String]
-    final case class Matches(r: Regex) extends StringP[String]
+  sealed trait NumType[A] {
+    def numeric: Numeric[A]
   }
-
-  // 
-  sealed trait NumType[A]
   object NumType {
-    implicit case object IntType extends NumType[Int]
-    implicit case object DoubleType extends NumType[Double]
-    implicit case object FloatType extends NumType[Float]
-    implicit case object LongType extends NumType[Long]
-    implicit case object ShortType extends NumType[Short]
-    implicit case object BigDecimalType extends NumType[BigDecimal]
-    implicit case object BigIntType extends NumType[BigInt]
+    implicit case object IntType extends NumType[Int] {
+      def numeric: Numeric[Int] = implicitly[Numeric[Int]]
+    }
+    implicit case object DoubleType extends NumType[Double] {
+      def numeric: Numeric[Double] = implicitly[Numeric[Double]]
+    }
+    implicit case object FloatType extends NumType[Float] {
+      def numeric: Numeric[Float] = implicitly[Numeric[Float]]
+    }
+    implicit case object LongType extends NumType[Long] {
+      def numeric: Numeric[Long] = implicitly[Numeric[Long]]
+    }
+    implicit case object ShortType extends NumType[Short] {
+      def numeric: Numeric[Short] = implicitly[Numeric[Short]]
+    }
+    implicit case object BigDecimalType extends NumType[BigDecimal] {
+      def numeric: Numeric[BigDecimal] = implicitly[Numeric[BigDecimal]]
+    }
+    implicit case object BigIntType extends NumType[BigInt] {
+      def numeric: Numeric[BigInt] = implicitly[Numeric[BigInt]]
+    }
   }
 
-  // A => Boolean
-  sealed trait NumP[A] {
-    def numType: NumType[A]
-  }
-  object NumP {
-    final case class GreaterThan[A](numType: NumType[A], value: A) extends NumP[A]
-    final case class LessThan[A](numType: NumType[A], value: A) extends NumP[A]
-    final case class EqualTo[A](numType: NumType[A], value: A) extends NumP[A]
+  // Predicates:
+
+  sealed trait Predicate[A]
+  object Predicate {
+    // String => Boolean
+    sealed trait Str[A] extends Predicate[A] //TODO maybe remove type param
+    object Str {
+      final case class MinLength(n: Int) extends Str[String]
+      final case class MaxLength(n: Int) extends Str[String]
+      final case class Matches(r: Regex) extends Str[String]
+    }
+
+    // A => Boolean
+    sealed trait Num[A] extends Predicate[A] {
+      def numType: NumType[A]
+    }
+    object Num {
+      final case class GreaterThan[A](numType: NumType[A], value: A) extends Num[A]
+      final case class LessThan[A](numType: NumType[A], value: A) extends Num[A]
+      final case class EqualTo[A](numType: NumType[A], value: A) extends Num[A]
+    }
+
+    final case class True[A]() extends Predicate[A] // A => True
   }
 
-  final case class Validation[Pred[_], A](bool: Bool[Pred[A]]) { self =>
-    def &&(that: Validation[Pred, A]): Validation[Pred, A] = Validation(self.bool && that.bool)
-    def ||(that: Validation[Pred, A]): Validation[Pred, A] = Validation(self.bool || that.bool)
-    def unary_! : Validation[Pred, A] = Validation(!self.bool)
+  final case class Validation[A](bool: Bool[Predicate[A]]) { self =>
+    def &&(that: Validation[A]): Validation[A] = Validation(self.bool && that.bool)
+    def ||(that: Validation[A]): Validation[A] = Validation(self.bool || that.bool)
+    def unary_! : Validation[A] = Validation(!self.bool)
   }
   object Validation {
-    def minLength(n: Int): Validation[StringP, String] = Validation(Bool.Leaf(StringP.MinLength(n)))
+    import Predicate._
+    
+    // String operations
+    def minLength(n: Int): Validation[String] = Validation(Bool.Leaf(Str.MinLength(n)))
+    def maxLength(n: Int): Validation[String] = Validation(Bool.Leaf(Str.MaxLength(n)))
 
-    def maxLength(n: Int): Validation[StringP, String] = Validation(Bool.Leaf(StringP.MaxLength(n)))
+    // Numerical operations
+    def greaterThan[A](value: A)(implicit numType: NumType[A]): Validation[A] = Validation(Bool.Leaf(Num.GreaterThan(numType, value)))
+    def lessThan[A](value: A)(implicit numType: NumType[A]): Validation[A] = Validation(Bool.Leaf(Num.LessThan(numType, value)))
+    def equalTo[A](value: A)(implicit numType: NumType[A]): Validation[A] = Validation(Bool.Leaf(Num.EqualTo(numType, value)))
 
-    //TODO add num operations
+    def any[A]: Validation[A] = Validation(Bool.Leaf(Predicate.True[A]()))
+    def none[A]: Validation[A] = !any[A]
+
+    // TODO:
+    // all validations apply by glueing them together with && (start with any)
+    // have overloads that take an iterable (List, Set, etc)
+    def allOf[A](vs: Validation[A]*): Validation[A] = vs.foldLeft(any[A])(_ && _)
+    def allOf[A](vl: Iterable[Validation[A]]): Validation[A] = allOf(vl.toSeq:_*)
+
+    // glue with || (start with none)
+    def anyOf[A](vs: Validation[A]*): Validation[A] = vs.foldLeft(none[A])(_ || _)
+    def anyOf[A](vl: Iterable[Validation[A]]): Validation[A] = anyOf(vl.toSeq:_*)
+  }
+
+  object examples {
+    import Validation._
+
+    val nameValidation: Validation[String] = minLength(4) && maxLength(50)
+
+    val anyInt: Validation[Int] = anyOf(greaterThan(2), lessThan(4), equalTo(3))
+    val anyString: Validation[String] = anyOf(minLength(3), maxLength(10))
+
+    val all: Validation[String] = allOf(minLength(3), maxLength(4))
+    val allInt = allOf(greaterThan(2), lessThan(4), equalTo(3))
   }
 
 
@@ -83,7 +135,7 @@ object ZioSchemaValidation {
   //   // 6) verify that strings represent/contain date-time information.
   //   // 7) cross-field validation
     
-  //   final case class Str(stringValidation: Bool[StringP]) extends Validation[String] { self =>
+  //   final case class Str(stringValidation: Bool[Str]) extends Validation[String] { self =>
   //     def &&(that: Str): Str = {
   //       Str(self.stringValidation && that.stringValidation)
   //     }
@@ -93,15 +145,17 @@ object ZioSchemaValidation {
   //     def unary_! = Str(!self.stringValidation)
   //   }
 
-  //   final case class Num[A](NumP: Bool[NumP[A]], numType: NumType[A]) extends Validation[A] { self =>
-  //     def &&(that: Num[A]): Num[A] = Num(self.NumP && that.NumP, that.numType)
-  //     def ||(that: Num[A]): Num[A] = Num(self.NumP || that.NumP, that.numType)
-  //     def unary_! : Validation[A] = Num(!self.NumP, self.numType)
+  //   final case class Num[A](Num: Bool[Num[A]], numType: NumType[A]) extends Validation[A] { self =>
+  //     def &&(that: Num[A]): Num[A] = Num(self.Num && that.Num, that.numType)
+  //     def ||(that: Num[A]): Num[A] = Num(self.Num || that.Num, that.numType)
+  //     def unary_! : Validation[A] = Num(!self.Num, self.numType)
   //   }
   // }
 
   // Problems:
   //  - no cross-field validation
+
+  // Need a validation that accepts anything
 
   // TODO
   // fork zio-schema
