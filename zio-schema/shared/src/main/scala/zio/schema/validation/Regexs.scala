3b36316c4b9deabe336c62d95ace230604ffbe8f
Marcin Szałomski <baldram@users.noreply.github.com>
2022-06-01 11:46:09 +0200

    #257: Add IPv4 validator (#275)
    * #257: Add IPv4 validator
    
    * #257: The final clean up

--- zio-schema/shared/src/main/scala/zio/schema/validation/Regexs.scala
+++ zio-schema/shared/src/main/scala/zio/schema/validation/Regexs.scala
@@ -21,6 +21,23 @@ trait Regexs {
     )
   }
 
+  /**
+   * Checks whether a certain string represents a valid IPv4 address.
+   */
+  lazy val ipV4: Validation[String] = {
+    val separator           = Regex.literal(".").exactly(1)
+    val isZeroOrOne: Regex  = Regex.oneOf('0', '1')
+    val isZeroToFour: Regex = isZeroOrOne | Regex.oneOf('2', '3', '4')
+    val isZeroToFive: Regex = isZeroToFour | Regex.oneOf('5')
+    val is250To255: Regex   = Regex.literal("25") ~ isZeroToFive
+    val is200To249: Regex   = Regex.literal("2") ~ isZeroToFour ~ Regex.digit
+    val isZeroTo199: Regex  = (isZeroOrOne ~ Regex.digit.exactly(2)) | Regex.digit.atMost(2)
+
+    val bytePart = is250To255 | is200To249 | isZeroTo199
+
+    Validation.regex(bytePart ~ separator ~ bytePart ~ separator ~ bytePart ~ separator ~ bytePart)
+  }
+
   object PhoneNumberRegexs {
     val optionalSpace: Regex     = Regex.literal(" ").atMost(1)
     val optionalSeparator: Regex = Regex.oneOf('-', ' ').atMost(1)
