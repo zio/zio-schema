15b1a786e0dec250afce81eeec789f3f8d3f51a0
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2022-11-08 22:03:35 +0100

    Fixes

--- zio-schema-avro/shared/src/test/scala-2/zio/schema/codec/AvroCodecSpec.scala
+++ zio-schema-avro/shared/src/test/scala-2/zio/schema/codec/AvroCodecSpec.scala
@@ -1394,9 +1394,9 @@ object AvroCodecSpec extends ZIOSpecDefault {
               """[{"type":"record","name":"A","fields":[]},{"type":"record","name":"B","fields":[]},{"type":"record","name":"MyC","fields":[]}]"""
             val schema = AvroCodec.decode(Chunk.fromArray(s.getBytes()))
 
-            val assertionA   = hasKey("A", tuple2First(isStandardType(StandardType.UnitType)))
-            val assertionB   = hasKey("B", tuple2First(isStandardType(StandardType.UnitType)))
-            val assertionMyC = hasKey("MyC", tuple2First(isStandardType(StandardType.UnitType)))
+            val assertionA   = hasKey("A", tuple2First(isEmptyRecord))
+            val assertionB   = hasKey("B", tuple2First(isEmptyRecord))
+            val assertionMyC = hasKey("MyC", tuple2First(isEmptyRecord))
             assert(schema)(isRight(isEnum(enumStructure(assertionA && assertionB && assertionMyC))))
           },
           test("correct case codec for case class of ADT") {
@@ -1408,8 +1408,8 @@ object AvroCodecSpec extends ZIOSpecDefault {
               "A",
               tuple2First(isRecord(hasRecordField(hasLabel(equalTo("s"))) && hasRecordField(hasLabel(equalTo("b")))))
             )
-            val assertionB   = hasKey("B", tuple2First(isStandardType(StandardType.UnitType)))
-            val assertionMyC = hasKey("MyC", tuple2First(isStandardType(StandardType.UnitType)))
+            val assertionB   = hasKey("B", tuple2First(isEmptyRecord))
+            val assertionMyC = hasKey("MyC", tuple2First(isEmptyRecord))
             assert(schema)(isRight(isEnum(enumStructure(assertionA && assertionB && assertionMyC))))
           },
           test("unwrap nested union") {
@@ -1419,15 +1419,15 @@ object AvroCodecSpec extends ZIOSpecDefault {
 
             val nestedEnumAssertion = isEnum(
               enumStructure(
-                hasKey("A", tuple2First(isStandardType(StandardType.UnitType))) && hasKey(
+                hasKey("A", tuple2First(isEmptyRecord)) && hasKey(
                   "B",
-                  tuple2First(isStandardType(StandardType.UnitType))
+                  tuple2First(isEmptyRecord)
                 )
               )
             )
             val nestedEnumKey =
               hasKey("zio.schema.codec.avro.wrapper_hashed_n465006219", tuple2First(nestedEnumAssertion))
-            val cEnumKey = hasKey("C", tuple2First(isStandardType(StandardType.UnitType)))
+            val cEnumKey = hasKey("C", tuple2First(isEmptyRecord))
             val dEnumKey = hasKey("D", tuple2First(isRecord(hasRecordField(hasLabel(equalTo("s"))))))
             assert(schema)(isRight(isEnum(enumStructure(nestedEnumKey && cEnumKey && dEnumKey))))
           }
@@ -1842,6 +1842,16 @@ object AssertionHelper {
         case _                   => None
       },
       assertion
+    )
+
+  def isEmptyRecord[A]: Assertion[Schema[_]] =
+    Assertion.isCase[Schema[_], Schema[_]](
+      "EmptyRecord", {
+        case r: CaseClass0[_]                                                => Some(r)
+        case r @ GenericRecord(_, structure, _) if structure.toChunk.isEmpty => Some(r)
+        case _                                                               => None
+      },
+      anything
     )
 
   def isEnum[A](assertion: Assertion[Schema.Enum[A]]): Assertion[Schema[_]] =
