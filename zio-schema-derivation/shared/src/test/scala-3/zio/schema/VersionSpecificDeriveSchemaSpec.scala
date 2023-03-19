73896b94c2dac6753b5b353672f646dfdb1b6aaa
Matthieu Leclercq <matthieu@automat.ai>
2023-03-19 14:18:18 -0400

    add derived extension method on Schema.type.
    Allows to use the scala3 `derives` keyword.

--- zio-schema-derivation/shared/src/test/scala-3/zio/schema/VersionSpecificDeriveSchemaSpec.scala
+++ zio-schema-derivation/shared/src/test/scala-3/zio/schema/VersionSpecificDeriveSchemaSpec.scala
@@ -10,12 +10,32 @@ trait VersionSpecificDeriveSchemaSpec extends ZIOSpecDefault {
   }
 
   inline def verifyFieldName[F]: FieldNameVerifier[F] = new FieldNameVerifier[F]
-    
-    
+
   class FieldNameVerifier[F] {
      inline def apply[S <: String & scala.Singleton](name: S): Boolean =
        VerifyFieldNameMacro.verifyFieldName[F, S]
   }
 
-  def versionSpecificSuite = Spec.labeled("Scala 3 specific tests", Spec.empty)
+  import SchemaAssertions._
+
+  final case class AutoDerives(i: Int) derives Schema
+
+  def versionSpecificSuite = Spec.labeled(
+    "Scala 3 specific tests", 
+    suite("Derivation")(
+      test("correctly derives case class with `derives` keyword") {
+        val expected: Schema[AutoDerives] = Schema.CaseClass1(
+          TypeId.parse("zio.schema.VersionSpecificDeriveSchemaSpec.AutoDerives"),
+          field0 = Schema.Field(
+            "i",
+            Schema.Primitive(StandardType.IntType),
+            get0 = _.i,
+            set0 = (a, b: Int) => a.copy(i = b)
+          ),
+          AutoDerives.apply
+        )
+        assert(Schema[AutoDerives])(hasSameSchema(expected))
+      }
+    )
+  )
 }
