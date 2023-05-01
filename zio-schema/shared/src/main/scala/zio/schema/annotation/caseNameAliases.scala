33c84e7b0b8d352abc2e11b46a4886a117bc5176
imorozov <ivan.morozov@deliveroo.co.uk>
2023-05-01 15:26:54 +0100

    Add scaladoc to annotations

--- zio-schema/shared/src/main/scala/zio/schema/annotation/caseNameAliases.scala
+++ zio-schema/shared/src/main/scala/zio/schema/annotation/caseNameAliases.scala
@@ -1,3 +1,8 @@
 package zio.schema.annotation
 
+/**
+ * Annotation for specifying a list of aliases if the case name is somehow known by.
+ *
+ * @param aliases A variable-length sequence of Strings representing the aliases of the case.
+ */
 final case class caseNameAliases(aliases: String*) extends scala.annotation.StaticAnnotation
--- zio-schema/shared/src/main/scala/zio/schema/annotation/transientField.scala
+++ zio-schema/shared/src/main/scala/zio/schema/annotation/transientField.scala
@@ -1,3 +1,9 @@
 package zio.schema.annotation
 
+/**
+ * Annotation specifying that serialization should make no effort to encode
+ * the field to which the annotation is applied.
+ *
+ * This is the dual of `@optionalField`.
+ */
 final case class transientField() extends scala.annotation.StaticAnnotation
