f6041eebe97d78a92f74245439b30e45a252175f
Ondra Pelech <ondra.pelech@gmail.com>
2023-06-30 19:03:25 +0200

    Do Scala.js properly

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -198,7 +198,7 @@ object BuildHelper {
       name := s"$prjName",
       crossScalaVersions := Seq(Scala213, Scala212, Scala3),
       ThisBuild / scalaVersion := Scala213, //crossScalaVersions.value.head, //Scala3,
-      scalacOptions := compilerOptions(scalaVersion.value, optimize = !isSnapshot.value),
+      scalacOptions ++= compilerOptions(scalaVersion.value, optimize = !isSnapshot.value),
       libraryDependencies ++= compileOnlyDeps(scalaVersion.value) ++ Seq(
         "dev.zio" %%% "zio-test"     % zioVersion % Test,
         "dev.zio" %%% "zio-test-sbt" % zioVersion % Test
