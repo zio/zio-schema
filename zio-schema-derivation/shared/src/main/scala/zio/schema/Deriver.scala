f03f084d7be4649646cf986c4951ca27a2bbb954
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2023-11-18 14:17:28 +0100

    Fix

--- zio-schema-derivation/shared/src/main/scala/zio/schema/Deriver.scala
+++ zio-schema-derivation/shared/src/main/scala/zio/schema/Deriver.scala
@@ -1,6 +1,5 @@
 package zio.schema
 
-import scala.annotation.nowarn
 import scala.reflect.ClassTag
 
 import zio.Chunk
@@ -64,7 +63,7 @@ trait Deriver[F[_]] extends VersionSpecificDeriver[F] { self =>
 
   def derivePrimitive[A](st: StandardType[A], summoned: => Option[F[A]]): F[A]
 
-  @nowarn def derivePrimitiveAlias[A: ClassTag, U](st: StandardType[U], summoned: => Option[F[A]]): F[A] =
+  def derivePrimitiveAlias[A: ClassTag, U](st: StandardType[U], summoned: => Option[F[A]]): F[A] =
     deriveUnknown(summoned)
 
   def deriveOption[A](option: Schema.Optional[A], inner: => F[A], summoned: => Option[F[Option[A]]]): F[Option[A]]
