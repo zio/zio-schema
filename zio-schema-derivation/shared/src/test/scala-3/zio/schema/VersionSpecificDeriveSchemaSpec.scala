244546f270ed15d97b55680492cf910a17163027
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2024-01-30 07:30:43 +0100

    Derive description from scaladoc for Scala 3 (#646)

--- zio-schema-derivation/shared/src/test/scala-3/zio/schema/VersionSpecificDeriveSchemaSpec.scala
+++ zio-schema-derivation/shared/src/test/scala-3/zio/schema/VersionSpecificDeriveSchemaSpec.scala
@@ -2,9 +2,12 @@ package zio.schema
 
 import zio.Chunk
 import zio.test.*
-import zio.schema.annotation.simpleEnum
+import zio.schema.annotation.*
 
 trait VersionSpecificDeriveSchemaSpec extends ZIOSpecDefault {
+  /** ObjectWithDoc doc */
+  object ObjectWithDoc
+
   case class ContainerFields(field1: Option[String])
 
   object ContainerFields {
@@ -22,14 +25,26 @@ trait VersionSpecificDeriveSchemaSpec extends ZIOSpecDefault {
 
   final case class AutoDerives(i: Int) derives Schema
 
+  /** AutoDerives scaladoc */
+  final case class AutoDerivesWithDoc(i: Int) derives Schema
+
   enum Colour(val rgb: Int) {
     case Red extends Colour(0xff0000)
     case Green extends Colour(0x00ff00)
     case Blue extends Colour(0x0000ff)
   }
 
+  /** Colour scaladoc */
+  @caseName("Red")
+  enum ColourWithDoc(val rgb: Int) {
+    /**  Red scaladoc */
+    case Red extends ColourWithDoc(0xff0000)
+    case Green extends ColourWithDoc(0x00ff00)
+    case Blue extends ColourWithDoc(0x0000ff)
+  }
+
   def versionSpecificSuite = Spec.labeled(
-    "Scala 3 specific tests", 
+    "Scala 3 specific tests",
     suite("Derivation")(
       test("correctly derives case class with `derives` keyword") {
         val expected: Schema[AutoDerives] = Schema.CaseClass1(
@@ -47,7 +62,19 @@ trait VersionSpecificDeriveSchemaSpec extends ZIOSpecDefault {
       test("correctly assigns simpleEnum to enum") {
         val derived: Schema[Colour] = DeriveSchema.gen[Colour]
         assertTrue(derived.annotations == Chunk(simpleEnum(true)))
-      }
+      },
+      test("correctly adds scaladoc as description"){
+        val colourWithDoc: Schema[ColourWithDoc] = DeriveSchema.gen[ColourWithDoc]
+        val autoDerivesWithDoc: Schema[AutoDerivesWithDoc] = Schema[AutoDerivesWithDoc]
+        val objectWithDoc: Schema[ObjectWithDoc.type] = DeriveSchema.gen[ObjectWithDoc.type]
+        val redAnnotations = colourWithDoc.asInstanceOf[Schema.Enum[ColourWithDoc]].cases.find(_.id == "Red").get.schema.annotations.find(_.isInstanceOf[description])
+        assertTrue(
+          colourWithDoc.annotations.find(_.isInstanceOf[description]) == Some(description("/** Colour scaladoc */")),
+          //redAnnotations == Some(description("/** Red scaladoc */")), fix #651 to make this work
+          autoDerivesWithDoc.annotations.find(_.isInstanceOf[description]) == Some(description("/** AutoDerives scaladoc */")),
+          objectWithDoc.annotations.find(_.isInstanceOf[description]) == Some(description("/** ObjectWithDoc doc */")),
+          )
+      },
     )
   )
 }
