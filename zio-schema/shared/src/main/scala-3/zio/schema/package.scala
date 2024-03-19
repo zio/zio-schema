df41083066bbb77802cbfc63cdfd55ac6ba62956
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2024-03-19 14:07:44 +0100

    Add `orElse` to Schema for Scala 3 that returns a union type (#669)

--- zio-schema/shared/src/main/scala-3/zio/schema/package.scala
+++ zio-schema/shared/src/main/scala-3/zio/schema/package.scala
@@ -1,5 +1,27 @@
 package zio
 
+import scala.reflect.ClassTag
+
 package object schema {
   type Singleton = scala.Singleton
+
+  extension [A: ClassTag, B: ClassTag] (s: Schema.Fallback[A, B])
+    def merge: Schema[A | B] =
+      s.transform(_.merge, {
+        case left: A  => Fallback.Left(left.asInstanceOf[A])
+        case right: B => Fallback.Right(right.asInstanceOf[B])
+      })
+
+  extension [A: ClassTag] (s: Schema[A])
+    def orElse[B: ClassTag](s0: Schema[B]): Schema[A | B] =
+      Schema.Fallback(s, s0).merge
+
+  extension [A: ClassTag, B: ClassTag] (fallback: Fallback[A, B])
+    def merge: A | B =
+      fallback match {
+        case Fallback.Left(left) => left
+        case Fallback.Right(right) => right
+        case Fallback.Both(left, _) => left
+      }
+
 }
