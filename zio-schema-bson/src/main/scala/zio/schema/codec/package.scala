fffdab0b70b18e59a66942aad88cd3564980e822
Ba Le Xuan <g.lexuanba@gmail.com>
2024-05-17 19:53:07 +0200

    Support ObjectId in schema-bson (#673)

---
+++ zio-schema-bson/src/main/scala/zio/schema/codec/package.scala
@@ -0,0 +1,22 @@
+package zio.schema.codec
+
+import org.bson.types.ObjectId
+
+import zio.schema.{ Schema, TypeId }
+
+package object bson {
+  val ObjectIdTag = "$oid"
+
+  implicit val ObjectIdSchema: Schema[ObjectId] =
+    Schema.CaseClass1[String, ObjectId](
+      id0 = TypeId.fromTypeName("ObjectId"),
+      field0 = Schema.Field(
+        name0 = ObjectIdTag,
+        schema0 = Schema[String],
+        get0 = _.toHexString,
+        set0 = (_, idStr) => new ObjectId(idStr)
+      ),
+      defaultConstruct0 = new ObjectId(_)
+    )
+
+}
