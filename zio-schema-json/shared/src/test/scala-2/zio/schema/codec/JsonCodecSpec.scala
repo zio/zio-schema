9195263c42e481875914e0720449d603f9048e41
paulpdaniels <paulpdaniels@gmail.com>
2024-06-09 11:56:28 +0800

    add a gen test

--- zio-schema-json/shared/src/test/scala-2/zio/schema/codec/JsonCodecSpec.scala
+++ zio-schema-json/shared/src/test/scala-2/zio/schema/codec/JsonCodecSpec.scala
@@ -1,7 +1,10 @@
 package zio.schema.codec
 
+import java.nio.charset.StandardCharsets
 import java.time.{ ZoneId, ZoneOffset }
+
 import scala.collection.immutable.ListMap
+
 import zio.Console._
 import zio._
 import zio.json.JsonDecoder.JsonError
@@ -19,8 +22,6 @@ import zio.stream.{ ZPipeline, ZStream }
 import zio.test.Assertion._
 import zio.test.TestAspect._
 import zio.test._
-
-import java.nio.charset.StandardCharsets
 
 object JsonCodecSpec extends ZIOSpecDefault {
 
@@ -574,7 +575,7 @@ object JsonCodecSpec extends ZIOSpecDefault {
               |""".stripMargin
           )
         )
-      } @@ TestAspect.tag("stream", "decoding"),
+      },
       test("case class with option fields omitted when empty") {
         assertDecodes(
           WithOptionFields.schema,
@@ -604,14 +605,31 @@ object JsonCodecSpec extends ZIOSpecDefault {
         )
       },
       test("case class with empty option field is decoded by stream") {
-        ZStream
-          .fromChunk(charSequenceToByteChunk("[{\"name\":\"John\"}]"))
-          .via(ZPipeline.decodeCharsWith(StandardCharsets.UTF_8))
-          .via(JsonCodec.jsonDecoder(AllOptionalFields.schema).decodeJsonPipeline(JsonStreamDelimiter.Array))
-          .runCollect
-          .either
-          .map(results => assertTrue(results.isRight))
-      } // @@ TestAspect.tag("stream", "decoding")
+        val names     = Gen.option(Gen.elements("John", "Jane", "Jermaine", "Jasmine"))
+        val age       = Gen.option(Gen.int(1, 99))
+        val person    = names.zipWith(age)(AllOptionalFields(_, _, None))
+        val delimiter = Gen.elements(JsonStreamDelimiter.Array, JsonStreamDelimiter.Newline)
+        val codec     = JsonCodec.jsonEncoder(AllOptionalFields.schema)
+
+        check(Gen.chunkOfBounded(0, 3)(person), delimiter) {
+          (people, delim) =>
+            val indent        = if (delim == JsonStreamDelimiter.Array) Some(1) else None
+            val encodedPeople = people.map(p => codec.encodeJson(p, indent))
+            val encoded = delim match {
+              case JsonStreamDelimiter.Array =>
+                encodedPeople.mkString("[", ",", "]")
+              case JsonStreamDelimiter.Newline =>
+                encodedPeople.mkString("", "\n", "\n")
+            }
+
+            assertDecodesJsonStream(
+              AllOptionalFields.schema,
+              people,
+              charSequenceToByteChunk(encoded),
+              delim
+            )
+        }
+      } @@ TestAspect.jvmOnly
     ),
     suite("enums")(
       test("case name aliases - default") {
@@ -1477,6 +1495,21 @@ object JsonCodecSpec extends ZIOSpecDefault {
   ) = {
     val result = ZStream.fromChunk(chunk).via(JsonCodec.schemaBasedBinaryCodec[A](cfg)(schema).streamDecoder).runCollect
     assertZIO(result)(equalTo(Chunk(value)))
+  }
+
+  private def assertDecodesJsonStream[A](
+    schema: Schema[A],
+    value: Chunk[A],
+    chunk: Chunk[Byte],
+    delimiter: JsonStreamDelimiter
+  ) = {
+    val result = ZStream
+      .fromChunk(chunk)
+      .via(ZPipeline.decodeCharsWith(StandardCharsets.UTF_8))
+      .via(JsonCodec.jsonDecoder(schema).decodeJsonPipeline(delimiter))
+      .runCollect
+      .either
+    assertZIO(result)(isRight(equalTo(value)))
   }
 
   private def assertEncodesThenDecodesFallback[A, B](
