5c6f3281f0e5155f64c907057765fafbbad9ba2e
David Anyatonwu <davidanyatonwu@gmail.com>
2024-06-09 02:07:15 +0100

    fix config param not found

--- zio-schema-json/shared/src/test/scala-3/zio/schema/codec/DefaultValueSpec.scala
+++ zio-schema-json/shared/src/test/scala-3/zio/schema/codec/DefaultValueSpec.scala
@@ -1,12 +1,11 @@
 package zio.schema.codec
 
-import zio.Console._
 import zio._
 import zio.json.{ DeriveJsonEncoder, JsonEncoder }
 import zio.schema._
 import zio.test.Assertion._
 import zio.test.TestAspect._
-import zio.test.*
+import zio.test._
 
 object DefaultValueSpec extends ZIOSpecDefault {
   implicit val defaultConfig: JsonCodec.Config = JsonCodec.Config.default
@@ -19,7 +18,7 @@ object DefaultValueSpec extends ZIOSpecDefault {
   private val customSuite = suite("custom")(
     suite("default value schema")(
       test("default value at last field") {
-        val result = JsonCodec.jsonDecoder(Schema[WithDefaultValue], defaultConfig).decodeJson("""{"orderId": 1}""")
+        val result = JsonCodec.jsonDecoder(Schema[WithDefaultValue]).decodeJson("""{"orderId": 1}""")
         assertTrue(result.isRight)
       }
     )
@@ -30,5 +29,4 @@ object DefaultValueSpec extends ZIOSpecDefault {
   object WithDefaultValue {
     implicit lazy val schema: Schema[WithDefaultValue] = DeriveSchema.gen[WithDefaultValue]
   }
-
 }
