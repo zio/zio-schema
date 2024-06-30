fdc60ccd3bda1447d0e58c283319c9265bab98eb
pfeme <pablofeme@gmail.com>
2024-06-30 21:40:52 +0200

    add caseclass0 check

--- zio-schema-json/shared/src/main/scala/zio/schema/codec/JsonCodec.scala
+++ zio-schema-json/shared/src/main/scala/zio/schema/codec/JsonCodec.scala
@@ -378,7 +378,9 @@ object JsonCodec {
 
     private def enumEncoder[Z](schema: Schema.Enum[Z], cfg: Config, cases: Schema.Case[Z, _]*): ZJsonEncoder[Z] =
       // if all cases are CaseClass0, encode as a String
-      if (schema.annotations.exists(_.isInstanceOf[simpleEnum])) {
+      if (schema.annotations.exists(_.isInstanceOf[simpleEnum]) && cases.forall(
+            _.schema.isInstanceOf[Schema.CaseClass0[_]]
+          )) {
         val caseMap: Map[Z, String] =
           schema.nonTransientCases
             .map(
@@ -699,7 +701,9 @@ object JsonCodec {
       }.toMap
 
       // if all cases are CaseClass0, decode as String
-      if (cases.forall(_.schema.isInstanceOf[Schema.CaseClass0[_]])) {
+      if (parentSchema.annotations.exists(_.isInstanceOf[simpleEnum]) && cases.forall(
+            _.schema.isInstanceOf[Schema.CaseClass0[_]]
+          )) {
         val caseMap: Map[String, Z] =
           cases.map(case_ => case_.id -> case_.schema.asInstanceOf[Schema.CaseClass0[Z]].defaultConstruct()).toMap
         ZJsonDecoder.string.mapOrFail(
