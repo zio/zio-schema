212692788dea47e546cd72aed59bcadbef204300
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-01-23 17:22:50 +0100

    Update zio-json to 0.7.5 (#790)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -34,7 +34,7 @@ object BuildHelper {
   val Scala3: String   = versions("3.3")
 
   val zioVersion                   = "2.1.14"
-  val zioJsonVersion               = "0.7.4"
+  val zioJsonVersion               = "0.7.5"
   val zioPreludeVersion            = "1.0.0-RC37"
   val zioOpticsVersion             = "0.2.2"
   val zioBsonVersion               = "1.0.6"
