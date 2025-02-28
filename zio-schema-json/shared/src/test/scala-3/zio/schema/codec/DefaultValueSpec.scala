00eeb12ae0d9345b5f6f1030f6a88634841b3d21
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-02-28 13:15:59 +0100

    Add en example of custom enum schema with a JSON codec

--- zio-schema-json/shared/src/test/scala-3/zio/schema/codec/DefaultValueSpec.scala
+++ zio-schema-json/shared/src/test/scala-3/zio/schema/codec/DefaultValueSpec.scala
@@ -29,7 +29,7 @@ object DefaultValueSpec extends ZIOSpecDefault {
         val decoder = JsonCodec.jsonDecoder(Schema[Base])
         val value = BaseB("a", Inner(1))
         val json = """{"type":"BaseB","a":"a","b":{"i":1}}"""
-        assert(decoder.decodeJson(json))(equalTo(Right(value)))
+        assert(decoder.decodeJson(json))(equalTo(Right(value))) &&
         assert(encoder.encodeJson(value))(equalTo(json))
       }
     ),
@@ -40,7 +40,7 @@ object DefaultValueSpec extends ZIOSpecDefault {
         val encoder = JsonCodec.jsonEncoder(schema)
         val json = """["abc",1,true]"""
         val value = Chunk[Int | String | Boolean]("abc", 1, true)
-        assert(decoder.decodeJson(json))(equalTo(Right(value)))
+        assert(decoder.decodeJson(json))(equalTo(Right(value))) &&
         assert(encoder.encodeJson(value))(equalTo(json))
       },
       test("union type of enums") {
@@ -49,7 +49,17 @@ object DefaultValueSpec extends ZIOSpecDefault {
         val encoder = JsonCodec.jsonEncoder(schema)
         val json = """[{"res":{"Left":"Err1"}},{"res":{"Left":"Err21"}},{"res":{"Right":{"i":1}}}]"""
         val value = Chunk[Result](Result(Left(ErrorGroup1.Err1)), Result(Left(ErrorGroup2.Err21)), Result(Right(Value(1))))
-        assert(decoder.decodeJson(json))(equalTo(Right(value)))
+        assert(decoder.decodeJson(json))(equalTo(Right(value))) &&
+        assert(encoder.encodeJson(value))(equalTo(json))
+      },
+      test("union type of custom types") {
+        import UnionValue.given
+
+        val decoder = zio.json.JsonCodec[Map[String, UnionValue]].decoder
+        val encoder = zio.json.JsonCodec[Map[String, UnionValue]].encoder
+        val json = """{"a":1,"b":"toto","c":true,"d":null}"""
+        val value = Map("a" -> 1, "b" -> "toto", "c" -> true, "d" -> null)
+        assert(decoder.decodeJson(json))(equalTo(Right(value))) &&
         assert(encoder.encodeJson(value))(equalTo(json))
       }
     )
@@ -88,4 +98,21 @@ object DefaultValueSpec extends ZIOSpecDefault {
   case class BaseA(a: String) extends Base derives Schema
 
   case class BaseB(a: String, b: Inner) extends Base derives Schema
+
+  given Schema[Null] = Schema.option[Unit].transform[Null]({ _ => null }, { _ => None })
+
+  type UnionValue = Int | Boolean | String | Null
+
+  object UnionValue {
+    given Schema[UnionValue] = Schema.enumeration[UnionValue, CaseSet.Aux[UnionValue]](
+      TypeId.Structural,
+      CaseSet.caseOf[Int, UnionValue]("int")(_.asInstanceOf[Int])(_.asInstanceOf[UnionValue])(_.isInstanceOf[Int]) ++
+        CaseSet.caseOf[Boolean, UnionValue]("boolean")(_.asInstanceOf[Boolean])(_.asInstanceOf[UnionValue])(_.isInstanceOf[Boolean]) ++
+        CaseSet.caseOf[String, UnionValue]("string")(_.asInstanceOf[String])(_.asInstanceOf[UnionValue])(_.isInstanceOf[String]) ++
+        CaseSet.caseOf[Null, UnionValue]("null")(_.asInstanceOf[Null])(_.asInstanceOf[UnionValue])(_ == null),
+      annotations = Chunk(zio.schema.annotation.noDiscriminator())
+    )
+
+    given zio.json.JsonCodec[UnionValue] = JsonCodec.jsonCodec(Schema[UnionValue])
+  }
 }
