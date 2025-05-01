4e90599f6a6745480cf01ee98d05c94661b8d54d
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2025-05-01 12:46:07 +0200

    Move NameFormat to core, since it is a more general concept (#834)

--- zio-schema-json/shared/src/main/scala/zio/schema/codec/JsonCodec.scala
+++ zio-schema-json/shared/src/main/scala/zio/schema/codec/JsonCodec.scala
@@ -148,90 +148,6 @@ JsonCodec.Configuration makes it now possible to configure en-/decoding of empty
     val default: Configuration = Configuration()
   }
 
-  sealed trait NameFormat extends (String => String)
-
-  object NameFormat {
-    import java.lang.Character._
-
-    private def enforceCamelOrPascalCase(s: String, toPascal: Boolean): String =
-      if (s.indexOf('_') == -1 && s.indexOf('-') == -1) {
-        if (s.isEmpty) s
-        else {
-          val ch = s.charAt(0)
-          val fixedCh =
-            if (toPascal) toUpperCase(ch)
-            else toLowerCase(ch)
-          s"$fixedCh${s.substring(1)}"
-        }
-      } else {
-        val len             = s.length
-        val sb              = new StringBuilder(len)
-        var i               = 0
-        var isPrecedingDash = toPascal
-        while (i < len) isPrecedingDash = {
-          val ch = s.charAt(i)
-          i += 1
-          (ch == '_' || ch == '-') || {
-            val fixedCh =
-              if (isPrecedingDash) toUpperCase(ch)
-              else toLowerCase(ch)
-            sb.append(fixedCh)
-            false
-          }
-        }
-        sb.toString
-      }
-
-    private def enforceSnakeOrKebabCase(s: String, separator: Char): String = {
-      val len                      = s.length
-      val sb                       = new StringBuilder(len << 1)
-      var i                        = 0
-      var isPrecedingNotUpperCased = false
-      while (i < len) isPrecedingNotUpperCased = {
-        val ch = s.charAt(i)
-        i += 1
-        if (ch == '_' || ch == '-') {
-          sb.append(separator)
-          false
-        } else if (!isUpperCase(ch)) {
-          sb.append(ch)
-          true
-        } else {
-          if (isPrecedingNotUpperCased || i > 1 && i < len && !isUpperCase(s.charAt(i))) sb.append(separator)
-          sb.append(toLowerCase(ch))
-          false
-        }
-      }
-      sb.toString
-    }
-
-    case class Custom(f: String => String) extends NameFormat {
-      override def apply(memberName: String): String = f(memberName)
-    }
-
-    case object SnakeCase extends NameFormat {
-      override def apply(memberName: String): String = enforceSnakeOrKebabCase(memberName, '_')
-    }
-
-    case object CamelCase extends NameFormat {
-      override def apply(memberName: String): String =
-        enforceCamelOrPascalCase(memberName, toPascal = false)
-    }
-
-    case object PascalCase extends NameFormat {
-      override def apply(memberName: String): String =
-        enforceCamelOrPascalCase(memberName, toPascal = true)
-    }
-
-    case object KebabCase extends NameFormat {
-      override def apply(memberName: String): String = enforceSnakeOrKebabCase(memberName, '-')
-    }
-
-    case object Identity extends NameFormat {
-      override def apply(memberName: String): String = memberName
-    }
-  }
-
   sealed trait DiscriminatorSetting
 
   object DiscriminatorSetting {
@@ -824,7 +740,7 @@ JsonCodec.Configuration makes it now possible to configure en-/decoding of empty
       } else {
         val encoders = nonTransientFields.map(field => schemaEncoder(field.schema.asInstanceOf[Schema[Any]], cfg))
         def name(field: Schema.Field[_, _]): String =
-          if (cfg.fieldNameFormat == JsonCodec.NameFormat.Identity) field.fieldName
+          if (cfg.fieldNameFormat == NameFormat.Identity) field.fieldName
           else if (field.fieldName == field.name) cfg.fieldNameFormat(field.fieldName)
           else field.fieldName
         val encodedNames =
@@ -1368,7 +1284,7 @@ JsonCodec.Configuration makes it now possible to configure en-/decoding of empty
       val nonTransientFields = schema.nonTransientFields.toArray.asInstanceOf[Array[Schema.Field[Z, Any]]]
       val encoders           = nonTransientFields.map(s => JsonEncoder.schemaEncoder(s.schema, cfg))
       def name(field: Schema.Field[_, _]): String =
-        if (cfg.fieldNameFormat == JsonCodec.NameFormat.Identity) field.fieldName
+        if (cfg.fieldNameFormat == NameFormat.Identity) field.fieldName
         else if (field.fieldName == field.name) cfg.fieldNameFormat(field.fieldName)
         else field.fieldName
       val encodedNames =
@@ -1889,7 +1805,7 @@ JsonCodec.Configuration makes it now possible to configure en-/decoding of empty
         fields(idx) = field
         decoders(idx) = schemaDecoder(field.schema, config)
         val name =
-          if (config.fieldNameFormat == JsonCodec.NameFormat.Identity) field.fieldName
+          if (config.fieldNameFormat == NameFormat.Identity) field.fieldName
           else if (field.fieldName == field.name) config.fieldNameFormat(field.fieldName)
           else field.fieldName
         names(idx) = name
